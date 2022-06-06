d5015a490e540794690fbccae464b92fb58176da
Arufuan <muhammad.alfan94@gmail.com>
2022-06-07 00:30:06 +0700

    commit KAPAL BAJA MOBILE APP

---
+++ android/app/src/main/kotlin/com/example/apsen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kb_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/about.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class About extends StatefulWidget {
+  @override
+  _AboutState createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(
+      //     'About',
+      //     style: TextStyle(fontSize: 12.0, color: Colors.black),
+      //   ),
+      // ), // ini untuk membuat titel atau judul suatu halaman
+      body: Container(
+        child: Center(
+          // center ini untuk meletakan object agar di tengah suatu halaman ya
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'gambar/logo-kb.png',
+                width: 80,
+                height: 80,
+              ), // ini fungsinya untuk menampilkan sebuah gambar ambil dari lokal kita
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'KAPAL BAJA APP',
+                style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight
+                        .bold), // oh ya untuk style disini seperti hanya  CSS di HTML
+              ),
+              SizedBox(
+                height: 5,
+              ), // ini dipakai untuk buat kasih jarak aja sih misalkan jarak dari atas kebawah pakai height untuk samping pakai width
+              Text(
+                'Versi : 0.1.8',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Muhammad Alfan Najih',
+                style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+              ),
+              Text(
+                '201810225093',
+                style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),
+        ),
+      ), //untuk body ini kalau di bahasa HTML itu element body <body> jadi wadang untuk membuat content yang akan di buat
+    );
+  }
+}
